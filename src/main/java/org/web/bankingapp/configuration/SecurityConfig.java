9775b32fc74be92ef6c1587ca22af42ebe59da2a
Alex <al1xbodeanu@gmail.com>
2024-12-15 12:13:13 +0100

    add

--- src/main/java/org/web/bankingapp/configuration/SecurityConfig.java
+++ src/main/java/org/web/bankingapp/configuration/SecurityConfig.java
@@ -16,17 +16,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.web.bankingapp.handler.CustomAccessDeniedHandler;
 import org.web.bankingapp.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig{
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -34,9 +39,11 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         //configurari
-                        .requestMatchers(HttpMethod.GET, "api/users/**").hasAnyRole("ADMIN", "USER")
-                        .requestMatchers("api/accounts/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/api/accounts/**").authenticated()
                         .anyRequest().permitAll())
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler(customAccessDeniedHandler))
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
--- src/main/java/org/web/bankingapp/security/JwtAuthenticationFilter.java
+++ src/main/java/org/web/bankingapp/security/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -29,23 +30,33 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        String header = request.getHeader("Authorization");
+        try{
+            String header = request.getHeader("Authorization");
 
-        if(StringUtils.isEmpty(header) || !header.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
+            if(StringUtils.isEmpty(header) || !header.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String jwt = header.substring("Bearer ".length());
+            String userName = jwtService.extractUserName(jwt);
+
+            if(!userName.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailService.loadUserByUsername(userName);
+                if(jwtService.isTokenValid(jwt, userDetails)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+        } catch (AccessDeniedException e) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("Access Denied in JWT Filter.");
             return;
         }
-        String jwt = header.substring("Bearer ".length());
-        String userName = jwtService.extractUserName(jwt);
 
-        if(!userName.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailService.loadUserByUsername(userName);
-            if(jwtService.isTokenValid(jwt, userDetails)){
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-            }
-        }
+
+
         filterChain.doFilter(request, response);
     }
+
 }
